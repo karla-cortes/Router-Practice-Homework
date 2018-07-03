49efcee142a29649c595820ceb602b69a76961f7
Karla Cortes <kmcortes7@gmail.com>
2018-07-03 17:02:27 -0400

    first commit

--- React_Routes/src/index.js
+++ React_Routes/src/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './CSS/index.css';
-
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Happy from './components/Happy';
 import Sleepy from './components/Sleepy';
 import Guilty from './components/Guilty';
@@ -10,7 +10,29 @@ import Guilty from './components/Guilty';
 class Home extends Component{
   render(){
     return(
-      // your code goes here
+      <Router>
+      <div>
+        <ul>
+          <li>
+          <Link to="/">Home</Link>
+          </li>
+          <li>
+          <Link to="/happy">Happy</Link>
+          </li>
+          <li>
+          <Link to="/sleepy">Sleepy</Link>
+          </li>
+          <li>
+          <Link to="/guilty">Guilty</Link>
+          </li>
+        </ul>
+        
+        <Route exact path="/" component={Home} />
+        <Route path="/happy" component={Happy} />
+        <Route path="/sleepy" component={Sleepy} />
+        <Route path="/guilty" component={Guilty} />
+      </div>
+      </Router>
     )
   }
 }
